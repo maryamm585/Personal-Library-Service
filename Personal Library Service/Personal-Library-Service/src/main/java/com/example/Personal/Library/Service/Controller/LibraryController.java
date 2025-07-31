af3f2ef9f0e925b69f70b688b9be4a6a0cf9674b
Mariam Mahmoud <146382004+MariamMahmoud2003@users.noreply.github.com>
2025-07-31 15:08:35 +0300

    Add files via upload

---
+++ Personal Library Service/Personal-Library-Service/src/main/java/com/example/Personal/Library/Service/Controller/LibraryController.java
@@ -0,0 +1,26 @@
+package com.luv2cod.library.Personal.Library.Service.Controller;
+
+import com.luv2cod.library.Personal.Library.Service.Config.LibraryConfig;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class LibraryController {
+    private final LibraryConfig libraryConfig;
+
+    public LibraryController(LibraryConfig libraryConfig) {
+        this.libraryConfig = libraryConfig;
+    }
+
+    @GetMapping("/library")
+    public Map<String, String> getLibraryInfo(){
+        Map<String, String> response = new LinkedHashMap<>();
+        response.put("name", libraryConfig.getName());
+        response.put("mode", libraryConfig.getMode());
+        return response;
+    }
+}
