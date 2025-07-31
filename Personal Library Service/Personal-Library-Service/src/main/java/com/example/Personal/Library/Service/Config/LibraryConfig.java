cf9f0c755b6ae6de6030f725166b506183a2cecf
maryamhossam <112872236+maryamm585@users.noreply.github.com>
2025-07-31 21:16:19 +0300

    package name alternation

--- Personal Library Service/Personal-Library-Service/src/main/java/com/example/Personal/Library/Service/Config/LibraryConfig.java
+++ Personal Library Service/Personal-Library-Service/src/main/java/com/example/Personal/Library/Service/Config/LibraryConfig.java
@@ -1,4 +1,4 @@
-package com.luv2cod.library.Personal.Library.Service.Config;
+package com.example.Personal.Library.Service.Config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
